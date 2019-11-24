bc5c69bfbac5fa007935ad3499a678d3dd4ea9a0
Zbyszek212322 <zbyszrad@poczta.wp.pl>
2019-11-24 15:32:54 +0100

    zmodyfikowany .gitignore

--- src/PartsFromLaminate/src/sample/Main.java
+++ src/PartsFromLaminate/src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Produkcja części z laminatu");
-        primaryStage.setScene(new Scene(root, 450, 300));
+        primaryStage.setScene(new Scene(root, 500, 300));
         primaryStage.show();
     }
 
